ed452a208fa26b9ec63bf0c4e0c14b011daf07fe
Senpaka <egorkazakov01012007@mail.ru>
2025-11-26 14:21:54 +0300

    Добавлены enum объекты

---
+++ MonopolyEngine/src/main/java/org/example/ru/vsu/oop/engine/model/board/ColorGroup.java
@@ -0,0 +1,12 @@
+package org.example.ru.vsu.oop.engine.model.board;
+
+public enum ColorGroup {
+    BROWN,
+    LIGHT_BLUE,
+    PINK,
+    ORANGE,
+    RED,
+    YELLOW,
+    GREEN,
+    DARK_BLUE
+}
---
+++ MonopolyEngine/src/main/java/org/example/ru/vsu/oop/engine/model/event/SpecialEffect.java
@@ -0,0 +1,12 @@
+package org.example.ru.vsu.oop.engine.model.event;
+
+public enum SpecialEffect {
+    PAY_MONEY,
+    RECEIVE_MONEY,
+    GO_TO_JAIL,
+    GO_TO_START,
+    SKIP_TURN,
+    MOVE_TO_CELL,
+    MOVE_BACK,
+    BONUS_ROLL,
+}
